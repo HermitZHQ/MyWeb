1830ef35556f6cd705a12aa6439e9cd948e654fa
DESKTOP-VO97DU0\z <cdzhanghongquan@gmail.com>
2017-03-16 18:28:23 +0800

    add novislider and test it

--- main.js
+++ main.js
@@ -1,17 +1,43 @@
-var container;
+var contentArea;
 var camera, scene, renderer;
 var mouseX = 0, mouseY = 0;
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
 var mod1 = new THREE.Object3D();
 
+// $(window).load(function () {
+//     $('#slider').nivoSlider({
+//         effect: 'random',
+//         slices: 15,
+//         boxCols: 8,
+//         boxRows: 4,
+//         animSpeed: 500,
+//         pauseTime: 3000,
+//         startSlide: 0,
+//         directionNav: true,
+//         controlNav: true,
+//         controlNavThumbs: false,
+//         pauseOnHover: true,
+//         manualAdvance: false,
+//         prevText: 'Prev',
+//         nextText: 'Next',
+//         randomStart: false,
+//         beforeChange: function () { },
+//         afterChange: function () { },
+//         slideshowEnd: function () { },
+//         lastSlide: function () { },
+//         afterLoad: function () { }
+//     });
+// });
+
 $(document).ready(function () {
-    //alert("main page ready");
+
     ParseParam();
-    //LoadObjTest();
 
     init();
     animate();
+
+    SliderFunc();
 
     $("button#toReg").click(function () {
         self.location.href = "register.html";
@@ -22,10 +48,16 @@ $(document).ready(function () {
     });
 })
 
+function SliderFunc() {
+    $("#slider").clientWidth = contentArea.clientWidth;
+    $("#slider").clientHeight = contentArea.clientWidth / 1.7777;
+    $("#slider").nivoSlider();
+}
+
 function init() {
     // container = document.createElement('div');
     // document.body.appendChild(container);
-    container = document.getElementById("contentArea");
+    contentArea = document.getElementById("contentArea");
 
     camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
     camera.position.z = 250;
@@ -37,7 +69,7 @@ function init() {
     var directionalLight = new THREE.DirectionalLight(0xffeedd);
     directionalLight.position.set(0, 0, 1);
     scene.add(directionalLight);
-    
+
     // texture
     var manager = new THREE.LoadingManager();
     manager.onProgress = function (item, loaded, total) {
@@ -53,7 +85,7 @@ function init() {
     };
 
     var onError = function (xhr) {
-        alert("Load model failed"); 
+        alert("Load model failed");
     };
 
     THREE.ImageUtils.crossOrigin = "anonymous";
@@ -85,8 +117,8 @@ function init() {
     renderer.setPixelRatio(window.devicePixelRatio);
     //renderer.setSize(window.innerWidth, window.innerHeight);
     //renderer.setSize(800, 600);
-    renderer.setSize(container.clientWidth, container.clientWidth/1.7777);
-    renderer.setClearColor(0xffffff,0);
+    renderer.setSize(contentArea.clientWidth, contentArea.clientWidth / 1.7777);
+    renderer.setClearColor(0xffffff, 0);
     //container.appendChild(renderer.domElement);
     $("#contentArea").prepend(renderer.domElement);
     // container.setAttribute("background-color", "#eeeeee");
@@ -102,7 +134,9 @@ function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     // renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setSize(container.clientWidth, container.clientWidth/1.7777);
+    renderer.setSize(contentArea.clientWidth, contentArea.clientWidth / 1.7777);
+    $("#slider").clientWidth = contentArea.clientWidth;
+    $("#slider").clientHeight = contentArea.clientWidth / 1.7777;
 }
 
 function onDocumentMouseMove(event) {
@@ -137,89 +171,4 @@ function ParseParam() {
 
         }
     }
-}
-
-function LoadObjTest() {
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
-    camera.position.z = 250;
-
-    //light
-    var ambient = new THREE.AmbientLight(0x101030);
-
-    scene.add(ambient);
-    var directionalLight = new THREE.DirectionalLight(0xffeedd);
-
-    directionalLight.position.set(0, 0, 1);
-
-    scene.add(directionalLight);
-
-    //renderer
-    var renderer = new THREE.WebGLRenderer();
-    var content = document.getElementById("contentArea");
-    renderer.setSize(800, 600);
-    content.appendChild(renderer.domElement);
-    //document.body.appendChild(renderer.domElement);
-
-    var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    var cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-
-    // var loader = new THREE.OBJLoader();
-    // // load a resource
-    // var obj = loader.load(
-    //     // resource URL
-    //     'resource/obj/male02.obj',
-    //     // Function when resource is loaded
-    //     function (object) {
-    //         scene.add(object);
-    //     }
-    // );
-
-    // texture
-    var manager = new THREE.LoadingManager();
-    manager.onProgress = function (item, loaded, total) {
-        console.log(item, loaded, total);
-    };
-    var texture = new THREE.Texture();
-    var onProgress = function (xhr) {
-        if (xhr.lengthComputable) {
-            var percentComplete = xhr.loaded / xhr.total * 100;
-            console.log(Math.round(percentComplete, 2) + '% downloaded');
-        }
-    };
-
-    var onError = function (xhr) {
-
-    };
-
-    var loader = new THREE.ImageLoader(manager);
-    loader.load('resource/textures/UV_Grid_Sm.jpg', function (image) {
-        texture.image = image;
-        texture.needsUpdate = true;
-    });
-
-    // model
-    var loader = new THREE.OBJLoader(manager);
-    loader.load('resource/obj/male02.obj', function (object) {
-        object.traverse(function (child) {
-            if (child instanceof THREE.Mesh) {
-                child.material.map = texture;
-            }
-        });
-        object.position.y = - 95;
-        scene.add(object);
-    }, onProgress, onError);
-
-    var render = function () {
-        requestAnimationFrame(render);
-
-        cube.rotation.x += 0.1;
-        cube.rotation.y += 0.1;
-
-        renderer.render(scene, camera);
-    };
-
-    render();
 }
